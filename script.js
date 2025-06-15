1f33e409e49b8c5475beaf5e41885b28873ce8c9
dev-rispoli <stenghelj@icloud.com>
2025-06-15 16:38:46 -0300

    corrigido nome do variavel

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const button = document.querySelector('.button');
-const massage = document.querySelector('.message');
+const message = document.querySelector('.message');
 
 button.addEventListener('click', () => {
-    if (massage.textContent === '') {
-        massage.textContent = 'Bem-vindo ao meu site! Que seu dia seja incrível e cheio de conquistas!';
+    if (message.textContent === '') {
+        message.textContent = 'Bem-vindo ao meu site! Que seu dia seja incrível e cheio de conquistas!';
     } else {
-        massage.textContent = '';
+        message.textContent = '';
     }
 });
